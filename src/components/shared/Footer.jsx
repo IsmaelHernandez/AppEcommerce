be1c77e00380ed3f746b38150948476e369b1ca5
Ismael <ismael@MacBook-Air-de-Ismael.local>
2022-09-08 09:27:30 -0500

    first commit

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <div className='footer__item'>
+            <h3>@Ismael hdz@2022</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
